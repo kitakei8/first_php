f6b1567fee40b8c3b00e57f86121959f5ab83dd2
Kei Kitamura <kitakei8@gmail.com>
2016-11-24 16:35:40 +0900

    tmp

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
 <?php
 
-$username = '';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $username = $_POST['username'];
-  $err = false;
-  if(strlen($username)>8) {
-    $err = true;
-  }
-}
+//setcookie("username", "taguchi");
+//setcookie("name", "kei", time()+60);
 
-?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>Check username</title>
-</head>
-<body>
-  <form action="" method="POST">
-    <input type="text" name="username" placeholder="user name" value="<?php echo htmlspecialchars($username, ENT_QUOTES, 'UTF-8') ?> ">
-    <input type="submit" value="Check!">
-    <?php if($err) { echo "Too Long!"; } ?>
-  </form>
-</body>
-</html>
+echo $_COOKIE['name'];
