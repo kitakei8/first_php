518dfd0fa720fde88e3c53c46dbb98fd45ac7e04
Kei Kitamura <kitakei8@gmail.com>
2016-11-25 17:23:51 +0900

    finish bot_php lesson

--- bot_php/bot.php
+++ bot_php/bot.php
@@ -1,3 +1,27 @@
 <?php
 
 require_once(__DIR__ . '/config.php');
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+
+$connection = new TwitterOAuth(
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+  ACCESS_TOKEN,
+  ACCESS_TOKEN_SECRET);
+//$content = $connection->get("account/verify_credentials");
+
+$media = $connection->upload("media/upload", [
+  'media' => __DIR__ . '/sakura.png'
+]);
+
+$res = $connection->post("statuses/update", [
+  'status' => 'botから画像投稿のテストです！',
+  'media_ids' => $media->media_id
+]);
+
+if ($connection->getLastHttpCode() === 200) {
+    echo 'Success!' . PHP_EOL;
+} else {
+    echo 'Error!' . $res->errors[0]->messages . PHP_EOL;
+}
--- bot_php/config.php
+++ bot_php/config.php
@@ -1,6 +1,8 @@
 <?php
 
+require_once(__DIR__ . '/vendor/autoload.php');
+
 define('CONSUMER_KEY', 'zDAieTg3u1yh1gbUB8PaWpJll');
 define('CONSUMER_SECRET', 'ARU8CuRfbrD5nhMWUbcEzGi0b5KxVACeT0nnZHKQqbLS4Exocv');
-define('ACCESS_TOKEN', '	781756558300426240-fIb2wHal1mfhFrywm68Wxk4QKgzKtsZ');
+define('ACCESS_TOKEN', '781756558300426240-fIb2wHal1mfhFrywm68Wxk4QKgzKtsZ');
 define('ACCESS_TOKEN_SECRET', '9YdBSZaGzRpf8zuXEelVxaL8rQckpMzuGVg0lLTLRPV4d');
